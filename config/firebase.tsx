042bbcb3804881a7c33b2f9eaac62c73238c7024
phuoc-huynh-ep <phuoc.huynh@expresspedia.net>
2021-07-01 10:06:18 +0700

    first commit

---
+++ config/firebase.tsx
@@ -0,0 +1,16 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyB1eOCXukdARPjERB8aFu9CICgRb7vGcoI",
+    authDomain: "cashbacksummit-auth.firebaseapp.com",
+    projectId: "cashbacksummit-auth",
+    storageBucket: "cashbacksummit-auth.appspot.com",
+    messagingSenderId: "895978286473",
+    appId: "1:895978286473:web:eb81573e17b22be2b30558",
+    measurementId: "G-4LTQWZHV32"
+};
+
+export const SIGN_PROVIDER = {
+    FACEBOOK: 'facebook.com',
+    GOOGLE: 'google.com',
+    PASSWORD: 'password'
+}
+
